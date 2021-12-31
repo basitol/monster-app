ed8369f513d31500ed7cc837389e182ab59b836b
basitol <basitolaitan27@gmail.com>
2021-12-31 14:04:37 +0100

    Monster app in react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { Component } from "react";
 
 import { CardList } from "./component/card-list/card-list.component";
+
+import { SearchBox } from "./component/search-box/search-box.component";
 // import reactDom from "react-dom";
 
 class App extends Component {
@@ -10,8 +12,15 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
+
+    // this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
@@ -20,10 +29,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <input type="search" placeholder="Search monsters"></input>
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolidex</h1>
+        <SearchBox
+          placeholder="Search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
